d67f6a9b284d793be5de9c6d278e7650afb654f7
Maria Paz Botero <maria.paz2311@gmail.com>
2024-11-24 15:21:58 +0100

    maj changement dep

--- scripts_rhapsodie/changement_dep_conllu.py
+++ scripts_rhapsodie/changement_dep_conllu.py
@@ -36,7 +36,7 @@ def sans_flat_dev(liste_drafts_1, liste_drafts_2):
             sent_id_1 = meta_1.get("sent_id")
             meta_2 = sentence_2.metadata
             sent_id_2 = meta_2.get("sent_id")
-            
+            #print(sent_id_1, sent_id_2)
 
             if sent_id_1 in dico_correspondance and dico_correspondance[sent_id_1] == sent_id_2:
                 id_correspondance = {}
@@ -54,23 +54,25 @@ def sans_flat_dev(liste_drafts_1, liste_drafts_2):
                             # print(id_correspondance)
                             dico_grand_correspondance[sent_id_2] = id_correspondance
                             break
-    print(dico_grand_correspondance)
+    #print(dico_grand_correspondance)
     return liste_drafts_2, dico_grand_correspondance
 
-def changer_head(liste_drafts_2, dico_correspondance):
-    for draft, (cle, dico) in zip(liste_drafts_2, dico_correspondance.items()):
+def changer_head(liste_drafts_2, dico_correspondances):
+    for draft in liste_drafts_2:
         for sentence in draft:
             features = {token['id']: token for token in sentence}
             meta = sentence.metadata
             sent_id = meta.get("sent_id")
-            
-            if sent_id == cle:
-                for id_2, token_2 in features.items():
-                    head_old = token_2['head']
-                    if head_old in dico:
-                        token_2['head'] = dico[head_old]
-                        print(f"cambio {id_2} de {head_old} a {token_2['head']}")
-        return liste_drafts_2
+            #print(sent_id)
+            for cle, dico in dico_correspondances.items():
+                if sent_id == cle:
+                    print(sent_id, cle)
+                    for id_2, token_2 in features.items():
+                        head_old = token_2['head']
+                        if head_old in dico:
+                            token_2['head'] = dico[head_old]
+                            print(f"cambio {id_2} de {head_old} a {token_2['head']}")
+    return liste_drafts_2
                         
 def exporter_corpus(liste_drafts, output_dir, liste_filename):
     '''Exporta el corpus actualizado'''
@@ -81,18 +83,20 @@ def exporter_corpus(liste_drafts, output_dir, liste_filename):
         with open(output_path, 'w', encoding='utf-8') as f:
             for sentence in sentences:
                 f.write(sentence.serialize())
-                f.write('\n\n')  # Separar las oraciones con una línea vacía
+                f.write('\n\n')  
         print(f"Fichier exporté : {output_path}")
 
 def main():
-    corpus_1 = "/Users/maria23paz/Documents/Stage/Rhapsodie/Conllu_original_try1/"
+    corpus_1 = "/Users/maria23paz/Documents/Stage/Rhapsodie/Conllu_original/"
     corpus_2 = "/Users/maria23paz/Documents/Stage/Rhapsodie/Conllu_depues_try2/"
     corpus_output = "/Users/maria23paz/Documents/Stage/Rhapsodie/Conllu_dep/"
     liste_drafts_1, liste_filename_1 = obtenir_conllu(corpus_1)
     liste_drafts_2, liste_filename_2 = obtenir_conllu(corpus_2)
     drafts_finale, gran_dico = sans_flat_dev(liste_drafts_1, liste_drafts_2)
-    changer_head(drafts_finale, gran_dico)
-    # exporter_corpus(liste_drafts_finale, corpus_output, liste_filename_2)
+    # print(drafts_finale)
+    # print(gran_dico)
+    liste_drafts_finale = changer_head(drafts_finale, gran_dico)
+    exporter_corpus(liste_drafts_finale, corpus_output, liste_filename_2)
 
 if __name__ == "__main__":
     main()
